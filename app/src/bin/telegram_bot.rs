ef7ae821b5cc634dccf6ce6c901482984a3dff4a
Julien <juliencaron@protonmail.com>
2022-02-07 00:34:55 +0100

    feat: impl get_price in the telegram bot

--- app/src/bin/telegram_bot.rs
+++ app/src/bin/telegram_bot.rs
@@ -2,9 +2,11 @@ use dotenv::dotenv;
 use std::{env, error::Error};
 
 use teloxide::prelude::*;
+use teloxide::types::ParseMode::MarkdownV2;
 use teloxide::utils::command::BotCommand;
+use teloxide::utils::markdown::link;
 
-use app::utils::to_uppercase;
+use app::{exchanges, utils};
 
 // Command examples
 
@@ -29,16 +31,38 @@ async fn answer(
     command: Command,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     match command {
-        Command::Help => cx.answer(Command::descriptions()).await?,
+        Command::Help => {
+            log::info!("Ask for help");
+            cx.answer(Command::descriptions()).await?
+        }
         Command::Price(pair) => {
+            log::info!("Ask for price for {}", pair);
             let mut pair_iter = pair.split_whitespace();
 
             if let Some(symbol_1) = pair_iter.next() {
                 // If the 2nd symbol is missing, set "USDT" as default
                 let symbol_2 = pair_iter.next().unwrap_or("usdt");
-                let target = to_uppercase(&format!("{}{}", &symbol_1, &symbol_2));
-                let response = format!("You ask price for pair: {}.", target);
-                cx.answer(response).await?
+                let target = utils::to_uppercase(&format!("{}{}", &symbol_1, &symbol_2));
+                let client = utils::get_client();
+                let result = exchanges::binance::get_price(client, &target).await;
+
+                match result {
+                    Some(tick) => {
+                        let response = format!("The price of {} is {}", target, tick.price);
+                        cx.answer(response).await?
+                    }
+                    None => {
+                        let market_link = link(
+                            "https://www.binance.com/en/markets/coinInfo",
+                            "binance market",
+                        );
+                        let message = format!(
+                            "Could not fetch {} on Binance\\. Look in the {} if you can find it\\.",
+                            target, market_link
+                        );
+                        cx.answer(message).parse_mode(MarkdownV2).send().await?
+                    }
+                }
             } else {
                 let error = format!("Please add a symbol (eg: btc)");
                 cx.answer(error).await?
